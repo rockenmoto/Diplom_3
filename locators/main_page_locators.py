fb77d1c14fb76b0200101009a57766df14213b17
Kirill Belov <rockenmoto@gmail.com>
2024-04-10 20:33:02 -0600

    Добавление теста - Тест для проверки заказа авторизованным пользователем

--- locators/main_page_locators.py
+++ locators/main_page_locators.py
@@ -7,7 +7,12 @@ class MainPageLocators:
     modal_window_locator = [By.XPATH, ".//section[contains(@class,'Modal_modal_opened')]"]
     detail_title_in_modal_window_locator = [By.XPATH, './/h2[text()="Детали ингредиента"]']
     detail_info_modal_window_locator = [By.CLASS_NAME, "Modal_modal__statsList__6cEm5"]
+    total_sum_locator = [By.XPATH, ".//div[contains(@class,'totalContainer')]/p"]
     close_modal_window_button = [By.XPATH, ".//section[contains(@class,'Modal_modal_opened')]/div[1]/button"]
+
+    place_order_button_locator = [By.XPATH, './/button[text()="Оформить заказ"]']
+    order_modal_window = [By.XPATH, ".//div[contains(@class,'Modal_modal__container')]"]
+    order_being_prepared_text_locator = [By.XPATH, ".//p[text()='Ваш заказ начали готовить']"]
 
     order_feed_title_locator = [By.XPATH, ".//h1[text()='Лента заказов']"]
     constructor_title_locator = [By.XPATH, ".//h1[text()='Соберите бургер']"]
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,5 +1,4 @@
 from locators.main_page_locators import MainPageLocators
-from locators.header_page_locators import HeaderPageLocators
 
 
 class TestMainPage:
@@ -17,4 +16,10 @@ class TestMainPage:
 
     def test_adding_ingredient_for_order(self, main_page):
         main_page.adding_ingredient_for_order()
-        assert main_page.get_text_element(MainPageLocators.bun_counter_locator) == '2'
+        assert (main_page.get_text_element(MainPageLocators.bun_counter_locator) == '2'
+                and main_page.get_text_element(MainPageLocators.total_sum_locator) == '1976')
+
+    def test_ordering_with_auth_user(self, login, main_page):
+        main_page.click_on_element(MainPageLocators.place_order_button_locator)
+        assert (main_page.wait_for_element(MainPageLocators.order_modal_window)
+                and main_page.wait_for_element(MainPageLocators.order_being_prepared_text_locator))
