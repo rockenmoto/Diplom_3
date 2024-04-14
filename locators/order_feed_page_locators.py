1f8d5873bea7359984386faff3eb0cf309c2419e
Kirill Belov <rockenmoto@gmail.com>
2024-04-14 12:20:36 -0600

    Корректировка тестов

--- locators/order_feed_page_locators.py
+++ locators/order_feed_page_locators.py
@@ -3,9 +3,9 @@ from selenium.webdriver.common.by import By
 
 class OrderFeedPageLocators:
     order_list_locator = [By.XPATH, ".//ul[@class = 'OrderFeed_list__OLh59']/li[1]"]
-    modal_window_locator = [By.XPATH, ".//section[contains(@class,'opened')]/div[contains(@class,'container')"]
+    modal_window_locator = [By.XPATH, ".//section[contains(@class,'opened')]/div[1]"]
     modal_ingredient_title_locator = [By.XPATH, ".//p[text()='Cостав']"]
-    modal_ingredient_list_locator = [By.CSS_SELECTOR, ".//ul[@class = 'Modal_list__2sHWc']/li"]
+    modal_ingredient_list_locator = [By.XPATH, ".//ul[@class = 'Modal_list__2sHWc']"]
     last_order_id_locator = [By.XPATH, ".//ul[contains(@class,'OrderFeed_list__OLh59')]/li[1]/a/div[1]/p[1]"]
     order_feed_all_time_number_locator = [By.XPATH, ".//div[contains(@class,'1L6Iv')]/div[2]/p[2]"]
     order_feed_today_number_locator = [By.XPATH, ".//div[contains(@class,'1L6Iv')]/div[3]/p[2]"]
--- tests/test_order_feed_page.py
+++ tests/test_order_feed_page.py
@@ -1,42 +1,48 @@
+import allure
+
 from locators.main_page_locators import MainPageLocators
 from locators.order_feed_page_locators import OrderFeedPageLocators
 from locators.header_page_locators import HeaderPageLocators
 
 
 class TestOrderFeedPage:
+    @allure.title('Проверка открытия всплывающего окна с деталями при клике на заказ')
     def test_open_detail_window_order_true(self, header_page, order_feed_page):
         header_page.click_on_element(HeaderPageLocators.order_feed_locator)
         order_feed_page.click_on_element(OrderFeedPageLocators.order_list_locator)
-        assert (order_feed_page.wait_for_element(OrderFeedPageLocators.modal_ingredient_title_locator) and
-                order_feed_page.wait_for_element(OrderFeedPageLocators.modal_ingredient_list_locator))
+        assert (order_feed_page.wait_for_element(OrderFeedPageLocators.modal_window_locator)
+                and order_feed_page.wait_for_element(OrderFeedPageLocators.modal_ingredient_title_locator)
+                and order_feed_page.wait_for_element(OrderFeedPageLocators.modal_ingredient_list_locator))
 
-    def test_order_id_the_same_for_history_and_feed_true(self, header_page, main_page,
-                                                         login, personal_page, order_feed_page):
-        main_page.adding_ingredient_for_order()
-        main_page.click_on_element(MainPageLocators.checkout_button_locator)
-        main_page.close_modal_window()
-        personal_page.click_on_history_order_section()
-        last_order_id = personal_page.get_order_id()
-        header_page.click_on_element(HeaderPageLocators.order_feed_locator)
-        last_order_id_two = order_feed_page.get_order_id()
-        assert last_order_id == last_order_id_two
 
-    def test_all_time_counter_completed_increases(self, login, header_page, main_page, order_feed_page):
-        header_page.click_on_element(HeaderPageLocators.order_feed_locator)
-        before_alltime_counter = order_feed_page.get_text_element(
-            OrderFeedPageLocators.order_feed_all_time_number_locator)
-        before_today_counter = order_feed_page.get_text_element(
-            OrderFeedPageLocators.order_feed_today_number_locator)
-        header_page.click_on_element(HeaderPageLocators.main_logo_locator)
-        main_page.adding_ingredient_for_order()
-        main_page.click_on_element(MainPageLocators.checkout_button_locator)
-        main_page.close_modal_window()
-        header_page.click_on_element(HeaderPageLocators.order_feed_locator)
-        after_all_time_counter = order_feed_page.get_text_element(
-            OrderFeedPageLocators.order_feed_all_time_number_locator)
-        after_today_counter = order_feed_page.get_text_element(
-            OrderFeedPageLocators.order_feed_today_number_locator)
+def test_order_id_the_same_for_history_and_feed_true(self, header_page, main_page,
+                                                     login, personal_page, order_feed_page):
+    main_page.adding_ingredient_for_order()
+    main_page.click_on_element(MainPageLocators.checkout_button_locator)
+    main_page.close_modal_window()
+    personal_page.click_on_history_order_section()
+    last_order_id = personal_page.get_order_id()
+    header_page.click_on_element(HeaderPageLocators.order_feed_locator)
+    last_order_id_two = order_feed_page.get_order_id()
+    assert last_order_id == last_order_id_two
 
-        in_work_order = order_feed_page.get_text_element(OrderFeedPageLocators.in_work_locator)
-        assert (after_all_time_counter > before_alltime_counter and after_today_counter > before_today_counter
-                and after_all_time_counter in in_work_order)
+
+def test_all_time_counter_completed_increases(self, login, header_page, main_page, order_feed_page):
+    header_page.click_on_element(HeaderPageLocators.order_feed_locator)
+    before_alltime_counter = order_feed_page.get_text_element(
+        OrderFeedPageLocators.order_feed_all_time_number_locator)
+    before_today_counter = order_feed_page.get_text_element(
+        OrderFeedPageLocators.order_feed_today_number_locator)
+    header_page.click_on_element(HeaderPageLocators.main_logo_locator)
+    main_page.adding_ingredient_for_order()
+    main_page.click_on_element(MainPageLocators.checkout_button_locator)
+    main_page.close_modal_window()
+    header_page.click_on_element(HeaderPageLocators.order_feed_locator)
+    after_all_time_counter = order_feed_page.get_text_element(
+        OrderFeedPageLocators.order_feed_all_time_number_locator)
+    after_today_counter = order_feed_page.get_text_element(
+        OrderFeedPageLocators.order_feed_today_number_locator)
+
+    in_work_order = order_feed_page.get_text_element(OrderFeedPageLocators.in_work_locator)
+    assert (after_all_time_counter > before_alltime_counter and after_today_counter > before_today_counter
+            and after_all_time_counter in in_work_order)
