787f9b8db8c93326fe8156440a751afd96eb3ee0
Kirill Belov <rockenmoto@gmail.com>
2024-04-14 11:26:17 -0600

    Добавление аллюр тега

--- tests/test_forgot_pass_page.py
+++ tests/test_forgot_pass_page.py
@@ -1,14 +1,26 @@
 from locators.reset_pass_locators import ResetPassPageLocators
-from user_data import UserData
 from urls import Urls
+from user import User
 import allure
 
 
 class TestForgotPassPage:
+    user = User()
+    user_data = []
+
+    @classmethod
+    def setup_class(cls):
+        cls.user_data = cls.user.create_new_user()
+
+    @allure.title('Проверка перехода на страницу восстановления пароля по кнопке «Восстановить пароль»')
     def test_fill_email_and_click_to_restore_button_true(self, header_page, login_page,
                                                          forgot_pass_page, reset_pass_page):
         header_page.click_to_personal_account()
         login_page.click_to_restore_pass_button()
-        forgot_pass_page.fill_and_to_click_o_restore_button(UserData.email)
+        forgot_pass_page.fill_and_to_click_o_restore_button(self.user_data[0])
         assert reset_pass_page.wait_for_element(
             ResetPassPageLocators.save_button_locator) and reset_pass_page.get_url() == Urls.reset_pass_url
+
+    @classmethod
+    def teardown_class(cls):
+        cls.user.delete_user(cls.user_data[3])
