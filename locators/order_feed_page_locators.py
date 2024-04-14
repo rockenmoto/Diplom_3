2b05610e9bcec5dbc5c540cac78ff2e5f3aa5a48
Kirill Belov <rockenmoto@gmail.com>
2024-04-13 23:00:15 -0600

    Добавление теста

--- locators/order_feed_page_locators.py
+++ locators/order_feed_page_locators.py
@@ -7,3 +7,5 @@ class OrderFeedPageLocators:
     modal_ingredient_title_locator = [By.XPATH, ".//p[text()='Cостав']"]
     modal_ingredient_list_locator = [By.CSS_SELECTOR, ".//ul[@class = 'Modal_list__2sHWc']/li"]
     last_order_id_locator = [By.XPATH, ".//ul[contains(@class,'OrderFeed_list__OLh59')]/li[1]/a/div[1]/p[1]"]
+    order_feed_all_time_number_locator = [By.XPATH, ".//div[contains(@class,'1L6Iv')]/div[2]/p[2]"]
+    order_feed_today_number_locator = [By.XPATH, ".//div[contains(@class,'1L6Iv')]/div[3]/p[2]"]
--- tests/test_order_feed_page.py
+++ tests/test_order_feed_page.py
@@ -10,7 +10,8 @@ class TestOrderFeedPage:
         assert (order_feed_page.wait_for_element(OrderFeedPageLocators.modal_ingredient_title_locator) and
                 order_feed_page.wait_for_element(OrderFeedPageLocators.modal_ingredient_list_locator))
 
-    def test_get_order_id(self, header_page, main_page, login, personal_page, order_feed_page):
+    def test_order_id_the_same_for_history_and_feed_true(self, header_page, main_page,
+                                                         login, personal_page, order_feed_page):
         main_page.adding_ingredient_for_order()
         main_page.click_on_element(MainPageLocators.place_order_button_locator)
         main_page.close_modal_window()
@@ -19,3 +20,14 @@ class TestOrderFeedPage:
         header_page.click_on_element(HeaderPageLocators.order_feed_locator)
         last_order_id_two = order_feed_page.get_order_id()
         assert last_order_id == last_order_id_two
+
+    def test_counter_completed_increases(self, login, header_page, main_page, order_feed_page):
+        header_page.click_on_element(HeaderPageLocators.order_feed_locator)
+        before_sum = order_feed_page.get_text_element(OrderFeedPageLocators.order_feed_all_time_number_locator)
+        header_page.click_on_element(HeaderPageLocators.main_logo)
+        main_page.adding_ingredient_for_order()
+        main_page.click_on_element(MainPageLocators.place_order_button_locator)
+        main_page.close_modal_window()
+        header_page.click_on_element(HeaderPageLocators.order_feed_locator)
+        after_sum = order_feed_page.get_text_element(OrderFeedPageLocators.order_feed_all_time_number_locator)
+        assert after_sum > before_sum
