e9d7c9df96b24b0dbb92b20435bd27f4286fabf5
Kirill Belov <rockenmoto@gmail.com>
2024-04-14 11:38:52 -0600

    Добавление аллюр тега

--- tests/test_reset_pass_page.py
+++ tests/test_reset_pass_page.py
@@ -5,6 +5,7 @@ from user_data import UserData
 
 
 class TestResetPassPage:
+    @allure.title('Проверка клик по кнопке показать/скрыть пароль делает поле активным — подсвечивает его')
     def test_show_pass_field_true(self, reset_pass_page, header_page, login_page, forgot_pass_page):
         header_page.click_to_personal_account()
         login_page.click_to_restore_pass_button()
