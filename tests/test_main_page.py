c8df57b2ef513302467b22700785c3cae32d1018
Kirill Belov <rockenmoto@gmail.com>
2024-04-14 11:02:10 -0600

    Добавил создание и удаление юзера после тестов на главной странице

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,7 +1,15 @@
 from locators.main_page_locators import MainPageLocators
+from user import User
 
 
 class TestMainPage:
+    user = User()
+    user_data = []
+
+    @classmethod
+    def setup_class(cls):
+        cls.user_data = cls.user.create_new_user()
+
     def test_click_to_ingredient(self, main_page):
         main_page.click_to_ingredient()
         assert ((main_page.wait_for_element(MainPageLocators.modal_window_locator)
@@ -19,7 +27,13 @@ class TestMainPage:
         assert (main_page.get_text_element(MainPageLocators.bun_counter_locator) == '2'
                 and main_page.get_text_element(MainPageLocators.total_sum_locator) == '1976')
 
-    def test_ordering_with_auth_user(self, login, main_page):
+    def test_ordering_with_auth_user(self, header_page, main_page, login_page):
+        header_page.click_to_personal_account()
+        login_page.login(self.user_data[0], self.user_data[1])
         main_page.click_on_element(MainPageLocators.place_order_button_locator)
         assert (main_page.wait_for_element(MainPageLocators.order_modal_window)
                 and main_page.wait_for_element(MainPageLocators.order_being_prepared_text_locator))
+
+    @classmethod
+    def teardown_class(cls):
+        cls.user.delete_user(cls.user_data[3])
